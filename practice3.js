1c038bcad3ac960df6ab714cb4f8e64dff76ed37
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-02-09 16:40:22 +0200

    practice

---
+++ practice3.js
@@ -0,0 +1,25 @@
+let num = 2;
+let foo = 6;
+let bar = 4;
+let answer = num + foo + bar ;
+console.log(answer);
+
+let res = 12;
+console.log(res);
+res = '12';
+console.log(res);
+
+let are = 10;
+let a = 3;
+let m = are % a;
+console.log(m);
+
+let number = 10;
+number++;
+console.log(number);
+
+ 
+
+
+
+
---
+++ practice4.js
@@ -0,0 +1,37 @@
+                                                          // (בוליאני) מידע פרימיטבי  //
+// let num = null;
+
+
+// let yoni = true;
+// console.log(yoni);
+// let dvd = yoni.toString();
+// console.log(dvd);
+// yoni = null;
+// console.log(yoni);
+//                                                        //  עדיין לא ידוע מה הוא עושה  //
+// let rse = Symbol();
+// let srd = Symbol();
+// console.log(rse);
+// console.log(srd);
+
+let yoni = false;
+console.log(yoni);
+let dvd = yoni.toString();
+console.log(dvd);
+ 
+let yosi = true;
+console.log(yosi);
+yosi = null;
+console.log(yosi);
+
+let ari;
+console.log(ari);
+ari = null;
+console.log(ari);
+
+let foor = "hello world";
+let baar = typeof foor;
+console.log(baar);
+foor = 1;
+baar = typeof foor;
+console.log(baar);
